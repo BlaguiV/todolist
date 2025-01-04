c6e88722ecaa041b2797e06ce060daa0d6f7779a
Vlad Blagui <vladblagui20006@gmail.com>
2025-01-04 17:19:56 +0200

    Added edit button & conditional checks

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,15 +3,48 @@ import { useState } from "react";
 function ToDoList() {
   const [tasks, setTasks] = useState(["eat breakfast", "take a shower"]);
   const [newTask, setNewTask] = useState("");
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editedTask, setEditedTask] = useState("");
+  const [error, setError] = useState("");
 
   function handleInputChange(event) {
     setNewTask(event.target.value);
   }
 
+  function handleEditedInputChange(event) {
+    setEditedTask(event.target.value);
+  }
+
   function addTask() {
+    if (tasks.includes(newTask)) {
+      setError("This task is already exists!");
+      return;
+    }
     if (newTask.trim() !== "") {
       setTasks((t) => [...t, newTask]);
       setNewTask("");
+      setError("");
+    }
+  }
+
+  function editTask(index) {
+    setEditingIndex(index);
+    setEditedTask(tasks[index]);
+  }
+
+  function saveEditedTask() {
+    if (tasks.includes(editedTask)) {
+      setError("This task is already exists!");
+      return;
+    }
+    if (editedTask.trim() !== "") {
+      const updatedTasks = tasks.map((task, index) =>
+        index === editingIndex ? editedTask : task
+      );
+      setTasks(updatedTasks);
+      setEditingIndex(null);
+      setEditedTask("");
+      setError("");
     }
   }
 
@@ -35,10 +68,26 @@ function ToDoList() {
             Add
           </button>
         </div>
+        {editingIndex !== null ? (
+          <div>
+            <input
+              type="text"
+              value={editedTask}
+              onChange={handleEditedInputChange}
+            />
+            <button className="save-button" onClick={saveEditedTask}>
+              Save
+            </button>
+          </div>
+        ) : null}
+        {error && <div className="error-message">{error}</div>}
         <ol>
           {tasks.map((element, index) => (
             <li key={index}>
               <span className="text">{element}</span>
+              <button className="edit-button" onClick={() => editTask(index)}>
+                Edit
+              </button>
               <button
                 className="delete-button"
                 onClick={() => deleteTask(index)}
